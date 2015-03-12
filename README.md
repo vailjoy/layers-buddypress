9673a34b22492e85df1ab406ab6ed1b00b8dc84e
vail <vail@obox.co.za>
2015-03-12 12:53:46 +0100

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# layers-buddypress
+# BuddyPress Support for Layers
+
 Child theme for Layers to add support for BuddyPress content.
+
+# Requirements
+
+You must have Layers 1.0.4 or later installed, and BuddyPress 1.7 or later
+
+#Install
+
+* Go to Appearance > Themes and click Add New
+* Upload the child theme zip and Activate
+* Ensure your BuddyPress pages are set to the Default page template.
+
+ To learn more about extending this child theme with
+ custom styling or additional BuddyPress features, 
+ refer to the BuddyPress Codex.
+ 
+ [http://codex.buddypress.org/themes/](https://codex.buddypress.org/themes/)
+
+
---
+++ buddypress.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying a BuddyPress page
+ *
+ * @package Layers
+ * @since Layers 1.0.4
+ */
+
+get_header(); ?>
+<?php do_action( 'layers_before_buddypress_content' ); ?>
+<section id="post-<?php the_ID(); ?>" <?php post_class( 'content-main clearfix' ); ?>>
+    <div class="row">
+        <?php if( have_posts() ) : ?>
+            <?php while( have_posts() ) : the_post(); ?>
+                <article <?php layers_center_column_class(); ?>>
+                    <?php global $post, $layers_post_meta_to_display, $layers_page_title_shown; ?>
+						<?php if( !isset( $layers_page_title_shown ) ) { ?>
+							<header class="section-title large">
+								<?php if( 'post' == get_post_type() ) { ?>
+									<?php layers_post_meta( get_the_ID(), array( 'date' ) , 'h5', 'meta-info' ); ?>
+								<?php } // if post ?>
+								<h1 class="heading"><?php the_title(); ?></h1> 
+                                <?php do_action( 'layers_after_buddypress_title' ); ?>
+							</header>
+                            <?php do_action( 'layers_after_buddypress_header' ); ?>
+						<?php } ?>
+						<?php // Layers Featured Media
+						echo layers_post_featured_media( array( 'postid' => get_the_ID(), 'wrap_class' => 'thumbnail push-bottom', 'size' => 'large' ) ); ?>
+                        <div class="story">		
+                
+							<?php the_content(); ?>
+                            
+                            <?php wp_link_pages( array(
+                                'link_before'   => '<span>',
+                                'link_after'    => '</span>',
+                                'before'        => '<p class="inner-post-pagination">' . __('<span>Pages:</span>', 'ocmx'),
+                                'after'     => '</p>'
+                            )); ?>
+                
+                        </div>
+
+                </article>
+            <?php endwhile; // while has_post(); ?>
+        <?php endif; // if has_post() ?>     
+    </div>
+</section>
+<?php do_action( 'layers_after_buddypress_content' ); ?>
+<?php get_footer(); ?>
